6d08908696a5cf2acafd322a7372ab19e747fc3c
Ralph Giles <giles@thaumas.net>
2015-02-01 18:28:34 -0800

    Use range notation instead of the range() iterator.
    Silences unstable warning now that the replacement actually works.

--- src/mandel.rs
+++ src/mandel.rs
@@ -9,7 +9,7 @@ struct Complex {
 fn escape(c: Complex) -> i32 {
   let max = 256;
   let mut z = Complex { r: 0.0, i: 0.0 };
-  for iter in range(0, max) {
+  for iter in (0..max) {
     if z.r * z.r + z.i * z.i > 4.0 {
       return iter;
     }
@@ -26,9 +26,9 @@ fn main() {
   let jrange = 20;
   let dr = 3.0 / irange as f64;
   let di = 3.0 / jrange as f64;
-  for j in range(0, jrange) {
+  for j in (0..jrange) {
     let ci = di * j as f64 - 1.5;
-    for i in range(0, irange) {
+    for i in (0..irange) {
       let cr = dr * i as f64 - 2.25;
       let e = escape(Complex { r: cr, i: ci });
       if e > 255 {
