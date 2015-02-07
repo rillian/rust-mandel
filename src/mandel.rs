07b73fa299343128de7b4c4da68884a3b6599b57
Ralph Giles <giles@thaumas.net>
2015-02-07 01:28:26 -0800

    Save pgm version of the rendered fractal.

--- src/mandel.rs
+++ src/mandel.rs
@@ -1,6 +1,8 @@
 // Mandelbrot Set in Rust
 // Copyright 2014 Ralph Giles, GNU GPLv3
 
+use std::old_io::{BufferedWriter, File};
+
 struct Complex {
   r: f64,
   i: f64
@@ -22,10 +24,17 @@ fn escape(c: Complex) -> i32 {
 
 fn main() {
   println!("Hello Benoit!");
+
+  let file = File::create(&Path::new("mandel.pgm"));
+  let mut out = BufferedWriter::new(file);
+
   let irange = 40;
   let jrange = 20;
   let dr = 3.0 / irange as f64;
   let di = 3.0 / jrange as f64;
+
+  out.write_line(&format!("P5 {} {} 1", irange, jrange));
+
   for j in (0..jrange) {
     let ci = di * j as f64 - 1.5;
     for i in (0..irange) {
@@ -36,6 +45,7 @@ fn main() {
       } else {
         print!(" ");
       }
+      out.write_u8((e%2) as u8);
     }
     println!("");
   }
